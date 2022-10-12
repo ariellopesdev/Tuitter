bf66047d681fdbc3887faf853f3eb6692a501eb1
lubyelcuritiba <lubyel.lopes@gmail.com>
2022-10-12 02:28:02 -0300

    Html's Optimization
    Optimized html code in a more professional code.

--- index.php
+++ index.php
@@ -5,16 +5,44 @@
     <meta charset="utf-8">
     <meta http-equiv="pragma" content="no-cache" />
     <link href="styles.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave, do, meu, site">
+    <meta name="description" content="Descrição do meu website">
+    <meta name="author" content="Ariel Lopes">
 </head>
 
 <body>
-    <div class="logoLogin">
+    <section class="logo-introduce">
+        <div class="center">
+            <div class="w50 left">
+                <h1>tuitter</h1>
+                <p>Welcome to the new, smart, effective and tecnological social media. We present to
+                everyone, THE NEW WORLD!</p>
+            </div><!--w50 left-->
+        </div><!--center-->
+    </section><!--logo-introduce-->
+
+    <!--<div class="logoLogin">
+        
         <div class="logoIntroduce">
-            <p class="logo">tuitter</p> 
-            <p class="introduceSocial">Welcome to the new, smart, effective and tecnological social media. Tuitter presents to
-                    you, THE NEW WORLD!</p>
-        </div>
-        <div class="loginMainPage">
+            <p class="logo">tuitter</p>
+            <p class="introduceSocial">Welcome to the new, smart, effective and tecnological social media. We present to
+                everyone, THE NEW WORLD!</p>
+        </div>-->
+     <section class="formulary">
+        <div class="center">
+            <div class="w50 right">
+            <form>
+                <input class="email" placeholder="Email" type="email" name="email" size="15" maxlength="40">
+                <input class="email" placeholder="Password" type="password" name="password" size="15" maxlength="40">
+                <input class="submitSubscribe" type="submit" name="subscribe" value="Subscribe">
+                <a href="Login/Uptade Password - Tuitter.php">Did you forgot password?</a>
+                <input class="submitCreate" type="submit" name="create account" value="Create Account">
+            </form>
+            </div><!--w50 right-->
+        </div><!--center-->
+     </section><!--formulary-->   
+       <!-- <div class="loginMainPage">
             <form>
                 <p>
                     <input class="emailPass" placeholder="Email" type="email" name="email" size="15" maxlength="40">
@@ -36,11 +64,22 @@
         </div>
     </div>
     <div class="refIndexBlock">
-        <p><a class="refIndex" href="About us/About us - Tuitter.php" >About us</a></p>
+        <p><a class="refIndex" href="About us/About us - Tuitter.php">About us</a></p>
         <p><a class="refIndex" href="Contact us/Contact us - Tuitter.php">Contact us</a></p>
         <p><a class="refIndex" href="Work with us/Work with us - Tuitter.php">Work with us</a></p>
         <p><a class="refIndex" href="Partner Companies/Partner Companies - Tuitter.php">Partner Companies</a></p>
-    </div>
+    </div>-->
+    <footer>
+        <div class="center">
+            <ul>
+                <li><a class="refIndex" href="About us/About us - Tuitter.php">About us</a></li>
+                <li><a class="refIndex" href="Contact us/Contact us - Tuitter.php">Contact us</a></li>
+                <li><a class="refIndex" href="Work with us/Work with us - Tuitter.php">Work with us</a></li>
+                <li><a class="refIndex" href="Partner Companies/Partner Companies - Tuitter.php">Partner Companies</a></li>
+            </ul>
+        </div><!--center-->
+        <p>All rights reserved</p>
+    </footer>
 
 </body>
 
