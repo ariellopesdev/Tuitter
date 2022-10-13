1703a6706eef992f1bb06b487b11082036b36e20
lubyelcuritiba <lubyel.lopes@gmail.com>
2022-10-12 23:39:13 -0300

    Responsivity on Index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <title>Tuitter: Welcome to the new world!</title>
     <meta charset="utf-8">
     <meta http-equiv="pragma" content="no-cache" />
-    <link href="styles.css" rel="stylesheet" />
+    <link href="style/font-awesome.min.css" rel="stylesheet">
+    <link href="style/styles.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="palavras-chave, do, meu, site">
     <meta name="description" content="Descrição do meu website">
@@ -13,27 +14,30 @@
 
 <body>
     <section class="center">
-            <div class="w60 left logo-introduce">
-                <div class="text">
-                    <h1>tuitter</h1>
-                    <p>Welcome to the new, smart, effective and tecnological social media. We present to
-                        everyone, THE NEW WORLD!</p>
-                </div><!--text-->
-            </div><!--w60 left logo-introduce-->
-            <div class="w40 left formulary">
-                <form class="right">
-                    <input class="email" placeholder="Email" type="email" name="email" size="15" maxlength="40">
-                    <input class="email" placeholder="Password" type="password" name="password" size="15" maxlength="40">
-                    <input type="submit" name="subscribe" value="Subscribe">
-                    <a href="Login/Uptade Password - Tuitter.php">Did you forgot password?</a>
-                    <p></p>
-                    <input type="button" name="create.account" value="Create Account">
-                </form>
+        <div class="w60 left logo-introduce">
+            <div class="text">
+                <h1>tuitter</h1>
+                <p>Welcome to the new, smart, effective and tecnological social media. We present to
+                    everyone, THE NEW WORLD!</p>
             </div>
-            <!--w40 left formulary-->
-            <div class="clear"></div>
-    </section><!--center-->
-    <footer>
+            <!--text-->
+        </div>
+        <!--w60 left logo-introduce-->
+        <div class="w40 left formulary">
+            <form class="right">
+                <input class="email" placeholder="Email" type="email" name="email" size="15" maxlength="40">
+                <input class="email" placeholder="Password" type="password" name="password" size="15" maxlength="40">
+                <input type="submit" name="subscribe" value="Subscribe">
+                <a href="Login/Uptade Password - Tuitter.php">Did you forgot password?</a>
+                <p></p>
+                <input type="button" name="create.account" value="Create Account">
+            </form>
+        </div>
+        <!--w40 left formulary-->
+        <div class="clear"></div>
+    </section>
+    <!--center-->
+    <footer class="desktop">
         <div class="center">
             <ul>
                 <li><a href="About us/About us - Tuitter.php">About us</a></li>
@@ -45,6 +49,23 @@
         <!--center-->
         <p>Developed by Ariel Lopes - All rights reserved</p>
     </footer>
+    <!--desktop-->
+    <footer class="mobile">
+        <div class="center">
+            <div class="button">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
+            <ul>
+                <li><a href="About us/About us - Tuitter.php">About us</a></li>
+                <li><a href="Contact us/Contact us - Tuitter.php">Contact us</a></li>
+                <li><a href="Work with us/Work with us - Tuitter.php">Work with us</a></li>
+                <li><a href="Partner Companies/Partner Companies - Tuitter.php">Partner Companies</a></li>
+            </ul>
+        </div>
+        <!--center-->
+        <p>Developed by Ariel Lopes - All rights reserved</p>
+    </footer>
+    <!--mobile-->
 
 </body>
 
