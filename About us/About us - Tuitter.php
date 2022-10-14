13bc92162bb023b4cd2c7df0ae75d2b6caf85ad1
lubyelcuritiba <lubyel.lopes@gmail.com>
2022-10-14 19:18:55 -0300

    Created Css to About us

--- About us/About us - Tuitter.php
+++ About us/About us - Tuitter.php
@@ -4,8 +4,8 @@
     <title>Tuitter - About us</title>
     <meta charset="utf-8">
     <meta http-equiv="pragma" content="no-cache" />
-    <link href="style/font-awesome.min.css" rel="stylesheet">
-    <link href="../style/others.css" rel="stylesheet" />
+    <link href="../Style/font-awesome.min.css" rel="stylesheet">
+    <link href="../Style/others.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="palavras-chave, do, meu, site">
     <meta name="description" content="Descrição do meu website">
@@ -13,13 +13,21 @@
 </head>
 
 <body>
-    <header>Welcome to the new world!</header>
+    <header>
+        <img src="../logoOfficial.png" alt="Company_logo">
+        <h1>
+            Welcome to the new world!
+        </h1>
+    </header>
     <section class="center">
         <h1>About us</h1>
-        <p>We are a new company in this country, we are online since 2022, sharing our thougts, dreams, experience with
-            y'all</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro non aperiam quia, incidunt et praesentium ullam possimus eos dignissimos? Rem minus obcaecati ipsa unde culpa vero ipsum ratione aperiam quia!</p>
-    </section><!--center-->
+        <div>
+            <p>We are a new company in this country, we are online since 2022, sharing our thougts, dreams, experience with
+                y'all</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro non aperiam quia, incidunt et praesentium ullam possimus eos dignissimos? Rem minus obcaecati ipsa unde culpa vero ipsum ratione aperiam quia!</p>
+        </div>
+    </section>
+    <!--center-->
     <footer class="desktop">
         <nav class="center">
             <ul>
@@ -29,14 +37,17 @@
                 <li><a href="../Partner Companies/Partner Companies - Tuitter.php">Partner Companies</a></li>
                 <li><a href="../index.php">Get back to main page</a></li>
             </ul>
-        </nav><!--center-->
+        </nav>
+        <!--center-->
         <p>Developed by Ariel Lopes - All rights reserved</p>
-    </footer><!--desktop-->
+    </footer>
+    <!--desktop-->
     <footer class="mobile">
         <nav class="center">
             <div class="button">
                 <i class="fa fa-bars" aria-hidden="true"></i>
-            </div><!--button-->
+            </div>
+            <!--button-->
             <ul>
                 <li><a href="..//Main Page/Main Page - Tuitter.php">Login</a></li>
                 <li><a href="../Contact us/Contact us - Tuitter.php">Contact us</a></li>
@@ -44,9 +55,11 @@
                 <li><a href="../Partner Companies/Partner Companies - Tuitter.php">Partner Companies</a></li>
                 <li><a href="../index.php">Get back to main page</a></li>
             </ul>
-        </nav><!--center-->
+        </nav>
+        <!--center-->
         <p>Developed by Ariel Lopes - All rights reserved</p>
-    </footer><!--mobile-->
+    </footer>
+    <!--mobile-->
 </body>
 
 </html>
